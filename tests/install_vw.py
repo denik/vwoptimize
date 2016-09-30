191602bb9a1e45cf2288631ec544d8f1821b1760
Denis Bilenko <denis.bilenko@gmail.com>
2016-09-30 22:28:42 +0200

    travis: use cache for vw and pip

---
+++ tests/install_vw.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+import os
+
+
+def system(cmd):
+    if os.system(cmd):
+        sys.exit(1)
+
+
+if not os.path.exists('bincache/vw'):
+    system('git clone --depth 1 https://github.com/JohnLangford/vowpal_wabbit.git /tmp/vw')
+    system('cd /tmp/vw && make')
+    system('cp /tmp/vw/vowpalwabbit/vw bincache/')
+
+system('cp bincache/vw /usr/local/bin/')
