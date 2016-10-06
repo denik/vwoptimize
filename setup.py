b4a450b42a36f8306e66d6d6b12ca97da1ec3733
Denis Bilenko <denis.bilenko@gmail.com>
2016-10-06 17:31:01 +0200

    read version from vwoptimize.py; set it to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
+import re
+
+version = re.search(r"__version__\s*=\s*'(.*)'", open('vwoptimize.py').read(500), re.M).group(1)
+assert version
 
 setup(name='vwoptimize',
-      version='0.1',
+      version=version,
       description='Cross-validation and hyper-parameter search for Vowpal Wabbit',
       author='Denis Bilenko',
       author_email='denis.bilenko@gmail.com',
--- vwoptimize.py
+++ vwoptimize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Wrapper for Vowpal Wabbit that does cross-validation and hyper-parameter tuning"""
+__version__ = '0.2'
 import sys
 import os
 import optparse
