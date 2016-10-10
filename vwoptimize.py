6cd8f6f2beb14fb58a3817088a5a6268cb7281f2
Denis Bilenko <denis.bilenko@gmail.com>
2016-10-10 17:11:08 +0200

    set version to 0.3.1

--- vwoptimize.py
+++ vwoptimize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Wrapper for Vowpal Wabbit that does cross-validation and hyper-parameter tuning"""
-__version__ = '0.2'
+__version__ = '0.3.1'
 import sys
 import os
 import optparse
