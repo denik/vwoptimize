99f6097296af7218f624d2dceb39305632c0c70c
Denis Bilenko <denis.bilenko@gmail.com>
2018-01-24 18:03:53 +0100

    set version to 0.9.1dev

--- vwoptimize.py
+++ vwoptimize.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 """Wrapper for Vowpal Wabbit that does cross-validation and hyper-parameter tuning"""
-__version__ = '0.9.0dev'
+__version__ = '0.9.1dev'
 __gitdescribe__ = 'GIT'
 import sys
 import os
