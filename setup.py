0f0d8b88e6c824ea10c97ebdbebfa09f52b8cc0e
Denis Bilenko <denis.bilenko@gmail.com>
2018-03-01 17:59:30 +0100

    fix dist: include vwoptimizelib

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 import re
 
 version = re.search(r"__version__\s*=\s*'(.*)'", open('vwoptimize.py').read(500), re.M).group(1)
@@ -10,6 +10,6 @@ setup(name='vwoptimize',
       author='Denis Bilenko',
       author_email='denis.bilenko@gmail.com',
       url='https://github.com/denik/vwoptimize',
-      py_modules=['vwoptimize'],
+      packages=find_packages('.'),
       scripts=['vwoptimize.py']
       )
